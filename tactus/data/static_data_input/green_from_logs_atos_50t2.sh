501664b7dd6446c52380b2a949e762f79a33e11f
Denis Haumont <denis.haumont@meteo.be>
2026-09-11 14:07:45 +0000

    .green

--- tactus/data/static_data_input/green_from_logs_atos_50t2.sh
+++ tactus/data/static_data_input/green_from_logs_atos_50t2.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-export SOURCE_DIR=/ec/project/accord/tactus/
+export SOURCE_DIR=/ec/project/accord/tactus
 export GREEN=cy50t2/.green
 
-find $SCRATCH/deode  -type l | xargs ls -all | grep  $SOURCE_DIR | cut -d'>' -f2 > $GREEN
-find $HOME/deode_ecflow -name "*.1" | xargs grep $SOURCE_DIR | grep "| INFO" | cut -d'|' -f3 | grep "ln -sf" | cut -d' ' -f4 >> $GREEN
-find $HOME/deode_ecflow -name "*.1" | xargs grep $SOURCE_DIR | grep -v "| INFO" | grep "ln -s"|cut -d' ' -f6 >> $GREEN
-find $HOME/deode_ecflow -name "*.1" | xargs grep $SOURCE_DIR | grep  " cp " | cut -d'|' -f3 | cut -d' ' -f3 >> $GREEN
-find $HOME/deode_ecflow -name "*.1" | xargs grep $SOURCE_DIR | grep  "\->" | cut -d'>' -f2 >> $GREEN
+find $SCRATCH/tactus  -type l | xargs ls -all | grep  $SOURCE_DIR | cut -d'>' -f2 > $GREEN.tmp
+find $HOME/tactus_ecflow -name "*.1" | xargs grep $SOURCE_DIR | grep "| INFO" | cut -d'|' -f3 | grep "ln -sf" | cut -d' ' -f4 >> $GREEN.tmp
+find $HOME/tactus_ecflow -name "*.1" | xargs grep $SOURCE_DIR | grep -v "| INFO" | grep "ln -s"|cut -d' ' -f6 >> $GREEN.tmp
+find $HOME/tactus_ecflow -name "*.1" | xargs grep $SOURCE_DIR | grep  " cp " | cut -d'|' -f3 | cut -d' ' -f3 >> $GREEN.tmp
+find $HOME/tactus_ecflow -name "*.1" | xargs grep $SOURCE_DIR | grep  "\->" | cut -d'>' -f2 >> $GREEN.tmp
 
 
-echo "$SOURCE_DIR/climate/GMTED2010" >> $GREEN
-cat $GREEN |  sed -e "s|$SOURCE_DIR/||g" | sed -e "s|//|/|g"|sed -e "s/^ //g"| sort | uniq > $GREEN
+echo "$SOURCE_DIR/climate/GMTED2010" >> $GREEN.tmp
+cat $GREEN.tmp |  sed -e "s|$SOURCE_DIR/||g" | sed -e "s|//|/|g"|sed -e "s/^ //g"| sort | uniq > $GREEN
+rm $GREEN.tmp
 
