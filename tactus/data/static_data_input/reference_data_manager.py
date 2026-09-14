df8692a7597d3b258b1a70f5fcb734004428637d
Denis Haumont <76622294+dhaumont@users.noreply.github.com>
2026-09-14 12:39:59 +0200

    working again

--- tactus/data/static_data_input/reference_data_manager.py
+++ tactus/data/static_data_input/reference_data_manager.py
@@ -6,11 +6,9 @@ import os
 import sys
 from pathlib import Path
 
-from pathspec import PathSpec
-
 # ---- Helpers
 
-def append_file(files, file_path, filename, root_folder, verbose):
+def append_file_to_list(files, file_path, filename, root_folder, verbose):
     if  os.path.exists(file_path):
         file_size = os.path.getsize(file_path)
         relative_path = os.path.relpath(file_path, root_folder)
@@ -20,15 +18,16 @@ def append_file(files, file_path, filename, root_folder, verbose):
     else:
         print(f"Warning: skip {filename}: file not found in folder {root_folder}")
         
-def get_files_from_dir(root_folder, verbose):
+def get_dict_from_dir(input_folder, verbose):
     files = []
-    for root, dirs, filenames in os.walk(root_folder, followlinks=True):
+    for root, dirs, filenames in os.walk(input_folder, followlinks=True):
         for filename in filenames:
-            file_path = os.path.join(root, dirs, filename)
-            append_file(files, file_path, filename, root_folder, verbose)
-    return files
+            file_path = os.path.join(root, filename)
+            append_file_to_list(files, file_path, filename, input_folder, verbose)
+    
+    return {"folder": input_folder, "files": files}    
 
-def get_files_from_flat_list(input_file, root_folder, verbose):
+def get_dict_from_flat_list(input_file, root_folder, verbose):
     files = []
     print("get_files_from_flat_list:")
     print(f"input_file: {input_file}")
@@ -36,16 +35,10 @@ def get_files_from_flat_list(input_file, root_folder, verbose):
     print(f"lines: {lines}")
     for filename in lines:
         file_path = os.path.join(root_folder,filename)
-        append_file(files, file_path, filename, root_folder, verbose)
+        append_file_to_list(files, file_path, filename, root_folder, verbose)
         
-
-    return files
-
-def create_set(data):
-    files = set()
-    for file in data["files"]:
-       files.add(file["filename"])
-    return files
+    return {"folder": root_folder, "files": files}    
+   
    
 def write_json(data, output_json):
     
@@ -53,12 +46,6 @@ def write_json(data, output_json):
         json.dump(data, json_file, indent=4)
 
     print(f"JSON file '{output_json}' generated successfully.")
-
-
-def generate_json(files, output_json, root_folder):
-
-    data = {"folder": root_folder, "files": files}
-    write_json(data, output_json)
     
 def get_verification_key(file_path):
     """Return the key used to check file consistency."""
@@ -92,78 +79,70 @@ def report_as_git(missing_files, unknown_files, key_mismatch):
 
 # ---- Comparison functions
 
-def check_dir(json_file, folder_path, ignore_list, verbose):
-    """check_dir files listed in the JSON."""
-        
-    folder_files = get_files_from_dir(folder_path)
-        
-    with open(json_file, "r") as f:
-        data = json.load(f)
+def compare(item, other_data, not_found_files,mistmatch_files = None):    
+    relative_path = item["relative_path"]    
+    other = next((x for x in other_data["files"] if x["relative_path"] == relative_path), None)
     
-    diff(json_file,)
-    reference_folder = data["folder"]
-
-    expected_files = {f["relative_path"]: f for f in data["files"]}
+    if other == None:
+        not_found_files.append(relative_path)
+    elif mistmatch_files != None:
+        # Check if the file hash matches                    
+        actual_key = item["size"]
+        other_key  = other["size"]
+        if actual_key != other_key:
+            mistmatch_files.append((relative_path, other_key, actual_key))
+    
+def diff(data, other_data,verbose):
 
     missing_files = []
     unknown_files = []
     key_mismatch = []
 
     # Check if files in JSON are present on disk
-    for relative_path, expected in expected_files.items():
-        file_path = os.path.join(folder_path, relative_path)
-        if verbose:
-            print(file_path)
-        if not os.path.exists(file_path):
-            missing_files.append(relative_path)
-        else:
-            # Check if the file hash matches
-            actual_key = get_verification_key(file_path)
-            if actual_key != expected["size"]:
-                key_mismatch.append((relative_path, actual_key, expected["size"]))
-
-    # Check if there are files on disk not listed in the JSON
-    seen = set()
-    for root, _dirs, filenames in os.walk(folder_path, followlinks=True):
-        if ignore_list and ignore_list.match_file(root):
-            continue
-
-        if verbose:
-            print(root)
-        ino = root
-        if ino in seen:
-            print(f"# Warning: circular dependency detected: {root} ")
-            continue
-        seen.add(ino)
-
-        for filename in filenames:
-            relative_path = os.path.relpath(os.path.join(root, filename), folder_path)
-            if ignore_list and ignore_list.match_file(relative_path):
-                continue
-
-            if relative_path not in expected_files:
-                unknown_files.append(relative_path)
+    for item in data["files"]:
+        compare(item,other_data,missing_files,key_mismatch)
+        
+    for other_item in other_data["files"]:
+        compare(other_item,data,unknown_files)
+        
     return missing_files, unknown_files, key_mismatch
 
-def append(data,other_data,force_update):
-   files_set = create_set(data)
+def check_dir(json_file, dir, ignore_list, verbose):
+    """check_dir files listed in the JSON."""
+        
+    other_data = get_dict_from_dir(dir, verbose)
+        
+    with open(json_file, "r") as f:
+        data = json.load(f)
+    
+    missing_files, unknow_files, key_mistmatch = diff(data,other_data,verbose)
+    return missing_files, unknow_files, key_mistmatch
+
+def diff_json(json_file, other_json_file, verbose):
+    with open(json_file, "r") as f:
+        data = json.load(f)
+        
+    with open(other_json_file, "r") as f:
+        other_data = json.load(f)
+
+    return diff(data,other_data,verbose)
+
+def append_op(data,other_data,force_update):
    
    for other in other_data["files"]:
        relative_path = other["relative_path"]
-          
-       if not relative_path in files_set:
+       item = next((x for x in data["files"] if x["relative_path"] == relative_path), None)
+       if not item:
            data["files"].append(other)
    
-def remove(data,other_data,force_update):   
-   files_set = create_set(data)
+def remove_op(data,other_data,force_update):      
    
    for other in other_data["files"]:
        relative_path = other["relative_path"]
    
-       if not relative_path in files_set:
-            item = next((x for x in data["files"] if x["relative_path"] == relative_path), None)
-            if item:
-                data["files"].remove(item)
+       item = next((x for x in data["files"] if x["relative_path"] == relative_path), None)            
+       if item:
+            data["files"].remove(item)
    
 def list_content(json_file):
    with open(json_file, "r") as f:
@@ -191,10 +170,10 @@ def boolean_op(operation, json_file, other_json_file,result_json_file,force_upda
 # ---- high level
 
 def append(json_file, other_json_file, result_json_file, force_update):
-    boolean_op(append,json_file, other_json_file,result_json_file, force_update)
+    boolean_op(append_op,json_file, other_json_file,result_json_file, force_update)
 
 def remove(json_file, other_json_file,result_json_file, force_update):
-    boolean_op(remove,json_file, other_json_file, result_json_file, force_update)
+    boolean_op(remove_op,json_file, other_json_file, result_json_file, force_update)
 
 # ---- Main folder
 
@@ -202,16 +181,16 @@ def main():
     """Main entry point."""
     parser = argparse.ArgumentParser(description="File validation script.")
 
-    parser.add_argument(
-        "--from_list", help="Path to required file of allowed files and folders.", default=None
-    )
-    parser.add_argument("--checkdir", help="check directory against json", default=None)    
+    
+    parser.add_argument("--check_dir", help="check directory against json", default=None)    
     parser.add_argument("--append", help="other json file", default=None)
+    parser.add_argument("--diff", help="other json file", default=None)
     parser.add_argument("--remove", help="other json file", default=None)
     parser.add_argument("--json", help="JSON file", default=None)
     parser.add_argument("--from_dir", help="Create a json file with the content of the directory", default=None)
     parser.add_argument("--from_list", help="Create a json file with the content of the files in the list", default=None)
     parser.add_argument("--output", help="Output JSON file", default=None)
+    parser.add_argument("--root", help="Root folder", default=None)
     
     parser.add_argument("--force_update", action="store_true", help="verbose mode")
     parser.add_argument("--ignore_list", help="JSON file", default=None)
@@ -224,18 +203,21 @@ def main():
         sys.exit(1)
 
     if args.from_list:
-       files = get_files_from_flat_list(args.file_list, args.folder, args.verbose)
-       generate_json(files, args.json, args.folder,args.verbose)
+       data = get_dict_from_flat_list(args.file_list, args.root, args.verbose)
+       write_json(data, args.json)
     elif args.from_dir:
-       files = get_files_from_dir(args.from_dir, args.folder, args.verbose)
-       generate_json(files, args.json, args.folder,args.verbose)
-    elif args.checkdir:
+       data = get_dict_from_dir(args.from_dir, args.verbose)
+       write_json(data, args.json)
+    elif args.check_dir:
        missing_files, unknown_files, key_mismatch = check_dir(args.json, args.check_dir, args.ignore_list, args.verbose)
+       report_as_git(missing_files, unknown_files, key_mismatch)
+    elif args.diff:
+       missing_files, unknown_files, key_mismatch = diff_json(args.json, args.diff, args.verbose)
        report_as_git(missing_files, unknown_files, key_mismatch)
     elif args.append:
         append(args.json,args.append, args.output, args.force_update)
     elif args.remove:
-        remove(args.json,args.append, args.output, args.force_update)    
+        remove(args.json,args.remove, args.output, args.force_update)    
     else:
         list_content(args.json)
 
