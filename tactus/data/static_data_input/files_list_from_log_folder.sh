ecf57230d747f5f74e61f074beca771b0bf6c1bd
Denis Haumont <76622294+dhaumont@users.noreply.github.com>
2026-09-16 10:52:35 +0200

    improve logs

--- tactus/data/static_data_input/files_list_from_log_folder.sh
+++ tactus/data/static_data_input/files_list_from_log_folder.sh
@@ -2,8 +2,13 @@
 export LOG_DIR=$1
 export OUTPUT_FILE=$2
 export ROOT_DIR=$3
-
 export ECFLOW_NAME = ${HOME}/tactus_ecflow
+
+echo "files_list_from_log_folder.sh:"
+echo LOG_DIR=$LOG_DIR
+echo OUTPUT_FILE=$OUTPUT_FILE
+echo ROOT_DIR=$ROOT_DIR
+echo ECFLOW_NAME=$ECFLOW_NAME
 
 find ${LOG_DIR}  -type l | xargs ls -all | grep  ${ROOT_DIR} | cut -d'>' -f2 > ${OUTPUT_FILE}.tmp
 find ${ECFLOW_NAME} -name "*.1" | xargs grep ${ROOT_DIR} | grep "| INFO" | cut -d'|' -f3 | grep "ln -sf" | cut -d' ' -f4 >> ${OUTPUT_FILE}.tmp
