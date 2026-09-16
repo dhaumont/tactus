aaa7f740c8575e96468a73ecb5e50041cd5f65f1
Denis Haumont <76622294+dhaumont@users.noreply.github.com>
2026-09-16 12:37:15 +0200

    log and cleaning

--- tactus/data/static_data_input/check_leonardo_50t2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-export TARGET_DIR=/leonardo_work/DestE_330_26/users/SAN/static/DEODE
-export REFERENCE=cy49t2/reference.json
-python checker.py  --folder $TARGET_DIR --json $REFERENCE 
-
-
